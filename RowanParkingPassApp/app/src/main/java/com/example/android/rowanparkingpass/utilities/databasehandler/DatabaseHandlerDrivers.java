eef0ff4dbab448afc42642058d2ccbc98d959a32
tyandjel94 <tyandjel94@gmail.com>
2016-02-27 15:51:44 -0500

    Apache Legacy Update, DatabaseHandlerDrivers Update
    Updated gradle to support legacy apache code. Updated
    databasehandlerdrivers to work with the driver class. There was another
    field (lastName).

--- RowanParkingPassApp/app/src/main/java/com/example/android/rowanparkingpass/utilities/databasehandler/DatabaseHandlerDrivers.java
+++ RowanParkingPassApp/app/src/main/java/com/example/android/rowanparkingpass/utilities/databasehandler/DatabaseHandlerDrivers.java
@@ -19,7 +19,8 @@ public class DatabaseHandlerDrivers extends DatabaseHandlerBase {
     // Login Table Columns names
     private static final String KEY_ID = "id";
     private static final String KEY_DRIVER_ID = "driver_id";
-    private static final String KEY_FULL_NAME_STR = "full_name_str";
+    private static final String KEY_FIRST_NAME_STR = "first_name_str";
+    private static final String KEY_LAST_NAME_STR = "last_name_str";
     private static final String KEY_STREET_STR = "street_str";
     private static final String KEY_CITY_STR = "city_str";
     private static final String KEY_STATE_STR = "state_str";
@@ -35,7 +36,8 @@ public class DatabaseHandlerDrivers extends DatabaseHandlerBase {
         String CREATE_VISITOR_TABLE = "CREATE TABLE " + TABLE_DRIVERS + "("
                 + KEY_ID + " INTEGER PRIMARY KEY,"
                 + KEY_DRIVER_ID + " TEXT,"
-                + KEY_FULL_NAME_STR + " TEXT,"
+                + KEY_FIRST_NAME_STR + " TEXT,"
+                + KEY_FIRST_NAME_STR + " TEXT,"
                 + KEY_STREET_STR + " TEXT,"
                 + KEY_CITY_STR + " TEXT,"
                 + KEY_STATE_STR + " TEXT,"
@@ -61,7 +63,8 @@ public class DatabaseHandlerDrivers extends DatabaseHandlerBase {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(KEY_DRIVER_ID, uid); // UID
-        values.put(KEY_FULL_NAME_STR, fname); // Name
+        values.put(KEY_FIRST_NAME_STR, fname); // Name
+        values.put(KEY_LAST_NAME_STR,lname);// Last name
         values.put(KEY_STREET_STR, street); // Street
         values.put(KEY_CITY_STR, city); // City
         values.put(KEY_STATE_STR, state); // State
@@ -77,7 +80,8 @@ public class DatabaseHandlerDrivers extends DatabaseHandlerBase {
     public void updateUser(String uid, String fname, String lname, String street, String city, String state, String zip) {
         SQLiteDatabase db = this.getReadableDatabase();
         ContentValues values = new ContentValues();
-        values.put(KEY_FULL_NAME_STR, fname); // Name
+        values.put(KEY_FIRST_NAME_STR, fname); // First Name
+        values.put(KEY_LAST_NAME_STR, lname);//Last Name
         values.put(KEY_STREET_STR, street); // Street
         values.put(KEY_CITY_STR, city); // City
         values.put(KEY_STATE_STR, state); // State
@@ -100,12 +104,13 @@ public class DatabaseHandlerDrivers extends DatabaseHandlerBase {
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
             user.put(KEY_DRIVER_ID, cursor.getString(1));
-            user.put(KEY_FULL_NAME_STR, cursor.getString(2));
-            user.put(KEY_STREET_STR, cursor.getString(3));
-            user.put(KEY_CITY_STR, cursor.getString(4));
-            user.put(KEY_STATE_STR, cursor.getString(5));
-            user.put(KEY_ZIP_INT, cursor.getString(6));
-            rows.add(new Driver(user.get(KEY_DRIVER_ID), user.get(KEY_FULL_NAME_STR), user.get(KEY_STREET_STR), user.get(KEY_CITY_STR), user.get(KEY_STATE_STR), user.get(KEY_ZIP_INT)));
+            user.put(KEY_FIRST_NAME_STR, cursor.getString(2));
+            user.put(KEY_LAST_NAME_STR, cursor.getString(3));
+            user.put(KEY_STREET_STR, cursor.getString(4));
+            user.put(KEY_CITY_STR, cursor.getString(5));
+            user.put(KEY_STATE_STR, cursor.getString(6));
+            user.put(KEY_ZIP_INT, cursor.getString(7));
+            rows.add(new Driver(user.get(KEY_DRIVER_ID), user.get(KEY_FIRST_NAME_STR), user.get(KEY_LAST_NAME_STR), user.get(KEY_STREET_STR), user.get(KEY_CITY_STR), user.get(KEY_STATE_STR), user.get(KEY_ZIP_INT)));
             user.clear();
             cursor.moveToNext();
         }
