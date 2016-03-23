0e395d0180f0f1da53e7326778dce693a75e44e7
shoblock94 <shoblock94@msn.com>
2016-03-23 17:54:18 -0400

    Table display
    Pulls info correctly

--- www/TableDisplay.php
+++ www/TableDisplay.php
@@ -1,63 +1,4 @@
-﻿<?php
-
-require_once("common.php");
-
-if (isset($_SESSION['username'])) {
-    echo nl2br("Welcome to the member's area, " . $_SESSION['username'] . "!\n ");
-} else {
-    header("Location: login.php");
-    die("Redirecting..");
-}
-
-$query = "SELECT * FROM Requests Where status = 0 ORDER BY `time_stamp` DESC";
-
-try {
-            // These two statements run the query against your database table.
-            $stmt   = $db->prepare($query);
-            $result = $stmt->execute();
-
-            echo "Query successful";
-        }
-        catch (PDOException $ex) {
-            // Note: On a production website, you should not output $ex->getMessage().
-            // It may provide an attacker with helpful information about your code. 
-            die("Failed to run query: " . $ex->getMessage());
-        }
-
-
-     $row = $stmt->fetchAll();
-     echo json_encode($row[0]);
-
-
-     if($row)
-     {
-
-     }
-     else
-     {
-	echo "No requests found";
-	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-        die();
-
-     }
-
-
-
-     echo "</tr>";
-     for($i = 0; $i < count($row); $i++) 
-     {
-	echo "test";
-        echo "<td>" . $row['driver_id'] . "</td>";
-	echo "<td>" . $row['start_date'] . "</td>";
-	echo "<td>" . $row['end_date'] . "</td>";
-	echo "<td><a href='PassDetails.php?id=".$row['pass_id']."'>View Job</td>";
-     }
-
-        echo "</tr>";
-}
-echo “</table>”;*/
-?>
-<html>
+﻿<html>
    
    <head>
       <title>Rowan Parking</title>
@@ -73,3 +14,73 @@ echo “</table>”;*/
    </head>
 <a href="logout.php">Logout</a>
 </html>
+
+<?php
+
+require_once("common.php");
+
+if (isset($_SESSION['username'])) {
+    echo nl2br("\nWelcome to the member's area, " . $_SESSION['username'] . "!\n ");
+} else {
+    header("Location: login.php");
+    die("Redirecting..");
+}
+
+$query = "SELECT * FROM Requests Where status = 0 ORDER BY `time_stamp` DESC";
+
+try {
+            // These two statements run the query against your database table.
+            $stmt   = $db->prepare($query);
+            $result = $stmt->execute();
+
+            echo "Query successful";
+	    echo nl2br("\n\n\n");
+        }
+        catch (PDOException $ex) {
+            // Note: On a production website, you should not output $ex->getMessage().
+            // It may provide an attacker with helpful information about your code. 
+            die("Failed to run query: " . $ex->getMessage());
+        }
+
+
+     $row = $stmt->fetchAll();
+     echo json_encode($row[0]);
+
+     echo nl2br("\n\n\n");
+
+     if($row)
+     {
+
+     }
+     else
+     {
+	echo "No requests found";
+	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        die();
+
+     }
+
+
+
+     echo "<tr>";
+     for($i = 0; $i < count($row); $i++) 
+     {
+	$temp = $row[$i];
+	echo "test";
+
+
+
+        echo "<td>" . $temp['driver_id'] . "</td>\t";
+	echo "<td>" . $temp['start_date'] . "</td>\t";
+	echo "<td>" . $temp['end_date'] . "</td>\t";
+	echo "<td><a href='PassDetails.php?id=".$row['pass_id']."'>View Job</td></a>";
+	echo nl2br("\n");
+     }
+
+        echo "</tr>";
+
+
+
+
+?>
+
--- www/snd.bat
+++ www/snd.bat
@@ -1,4 +1,4 @@
-SET ADDRESS=150.250.173.54
+SET ADDRESS=150.250.173.131
 scp -l root -pw abc123 * %ADDRESS%:/var/RowanParkingPassApp/www
 cd css
 pause
