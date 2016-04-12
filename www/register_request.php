e06622101c0760020f92d29e7950b8756239d8bc
ILikesCaviar <rammcconnell@gmail.com>
2016-04-11 20:27:07 -0400

    Added files via upload

--- www/register_request.php
+++ www/register_request.php
@@ -14,47 +14,61 @@ die('{"FLAG":false,"ERR":8}');# No driver error
 
 if($_SESSION['user'] && $_POST && $_POST[':vehicle_id'] && $_POST[':driver_id'] && $_POST[':start_date'] && $_POST[':end_date']){
     die('{"FLAG":true,"id":1}');
-//die();
-// if vehicle exists send id and check unction table for user association
+	// check vehicle/driver exists
 	$query = "
             SELECT
-            *
+            1
             FROM Vehicles
 			WHERE
-            make=:make and model=:model and license=:license and state=:state and color=:color and year=:year";
-    $array_ids = array(':make'=>$_POST[':make'],
-	':model'=>$_POST[':model'],
-	':license'=>$_POST[':license'],
-	':state'=>($_POST[':state']),
-	':color'=>($_POST[':color']),
-	':year'=>($_POST[':year']));
+            vehicle_id=:vehicle_id";
+    $array_ids = array(':vehicle_id'=>$_POST[':vehicle_id']);
 	try {
         // These two statements run the query against your database table.
-        
         $stmt   = $db->prepare($query);
-		
         $result = $stmt->execute($array_ids);
-		
     }
     catch (PDOException $ex) {
         goto ERR;
     }
     $row  = $stmt->fetch();
-	if($row){
-		echo '{"FLAG":true,"id":'.$row['vehicle_id'].'}';
-	}else{
-		$query = "
-            INSERT INTO Vehicles
-            (make,model,license,state,color,year)
+	if(!$row){
+		echo '{"FLAG":false,"ERR":7}';
+		goto ERR;
+	}
+	$query = "
+            SELECT
+            1
+            FROM Driver
+			WHERE
+            driver_id=:driver_id";
+    $array_ids = array(':driver_id'=>$_POST[':driver_id']);
+	try {
+        // These two statements run the query against your database table.
+        $stmt   = $db->prepare($query);
+        $result = $stmt->execute($array_ids);
+    }
+    catch (PDOException $ex) {
+        goto ERR;
+    }
+    $row  = $stmt->fetch();
+	if(!$row){
+		echo '{"FLAG":false,"ERR":8}';
+		goto ERR;
+	}
+	// Check date conflicts
+	// add request
+	$query = "
+            INSERT INTO Requests
+            (vehicle_id,driver_id,user_id,status,start_date,end_date)
             VALUES
-			(:make,:model,:license,:state,:color,:year)";
-		$array_ids = array(':make'=>$_POST[':make'],
-		':model'=>$_POST[':model'],
-		':license'=>$_POST[':license'],
-		':state'=>intval($_POST[':state']),
-		':color'=>intval($_POST[':color']),
-		':year'=>intval($_POST[':year']));
-		try {
+			(:vehicle_id,:driver_id,:user_id,:status,:start_date,:end_date)";
+	$array_ids = array(':vehicle_id'=>$_POST[':vehicle_id'],
+		':driver_id'=>$_POST[':driver_id'],
+		':user_id'=>$_SESSION['user']['user_id'],
+		':status'=>intval(0),
+		':start_date'=>intval($_POST[':start_date']),
+		':end_date'=>intval($_POST[':end_date']));
+	try {
 			// These two statements run the query against your database table.
 			$stmt   = $db->prepare($query);
 			$result = $stmt->execute($array_ids);
@@ -65,12 +79,10 @@ if($_SESSION['user'] && $_POST && $_POST[':vehicle_id'] && $_POST[':driver_id'] 
 		}
 		$temp = $stmt->fetch(PDO::FETCH_ASSOC);
 		echo '{"FLAG":true,"id":'+json_encode($temp)+'}';
-		// insert grab key and send it back
-	}
 }else{
+	echo '{"FLAG":false,"ERR":2}';
 	goto ERR;
 }
 die();
 ERR:
-echo '{"FLAG":false}';
 header("HTTP/1.1 500 Internal Server Error");
