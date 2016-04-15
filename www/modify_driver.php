6479168a338ae967ff8aef860670fb372cbfb957
ILikesCaviar <rammcconnell@gmail.com>
2016-04-15 16:55:32 -0400

    Added files via upload

--- www/modify_driver.php
+++ www/modify_driver.php
@@ -1,123 +1,178 @@
 <?php
 require_once("common.php");
-
-if(empty($_SESSION['user']) ){
-    echo '{"FLAG":false,"ERR":2}';# param error
-    die("gooot");
-	goto ERR;
+if (empty($_SESSION['user'])) {
+    echo '{"FLAG":false,"ERR":2}'; # param error
+    goto ERR;
 }
-if(!empty($_POST[':driver_id']) && !empty($_POST['kill']) && $_POST['kill']=='1'){
 
-		$query = "
+$street    = trim($_POST[':street']);
+$city      = trim($_POST[':city']);
+$full_name = trim($_POST[':full_name']);
+$zip       = trim(strval($_POST[':zip']));
+$d_id      = trim($_POST[':driver_id']);
+
+if (isset($_POST[':street']) && empty($street)) {
+    echo '{"FLAG":false,"ERR":2,"PARAM":":street"}'; # param error
+    goto ERR;
+}
+if (isset($_POST[':city']) && empty($city)) {
+    echo '{"FLAG":false,"ERR":2,"PARAM":":city"}'; # param error
+    goto ERR;
+}
+if (isset($_POST[':full_name']) && empty($full_name)) {
+    echo '{"FLAG":false,"ERR":2,"PARAM":":full_name"}'; # param error
+    goto ERR;
+}
+if (isset($_POST[':zip']) && empty($zip)) {
+    echo '{"FLAG":false,"ERR":2,"PARAM":":zip"}'; # param error
+    goto ERR;
+}
+if (isset($_POST[':driver_id']) && empty($d_id)) {
+    echo '{"FLAG":false,"ERR":2,"PARAM":":driver_id"}'; # param error
+    goto ERR;
+}
+$zip = intval(strval($zip));
+
+if (!empty($d_id) && !empty($_POST['kill']) && trim($_POST['kill']) == '1') {
+    
+    $query     = "
             DELETE FROM Driver
 			WHERE driver_id=:driver_id";
-    $array_ids = array(':driver_id'=>$_POST[':driver_id']);
-	try {
+    $array_ids = array(
+        ':driver_id' => $d_id
+    );
+    try {
         // These two statements run the query against your database table.
         $stmt   = $db->prepare($query);
         $result = $stmt->execute($array_ids);
     }
     catch (PDOException $ex) {
-		echo '{"FLAG":false,"ERR":3}';# Database error
+        echo '{"FLAG":false,"ERR":3}'; # Database error
         goto ERR;
     }
-	die('{"FLAG":true}'); // delete success
-	}
-if($_POST[':street'] && $_POST[':city'] && $_POST[':full_name'] && $_POST[':zip'] && ctype_digit($_POST[':zip'])){
-	if(!$_POST[':driver_id']){
-	$query = "
+    die('{"FLAG":true}'); // delete success
+}
+
+if (empty($d_id)) {
+    $query = "
             SELECT
             *
             FROM Driver
 			WHERE
             street=:street and city=:city and full_name=:full_name and zip=:zip";
-    $array_ids = array(':street'=>$_POST[':street'],
-	':city'=>$_POST[':city'],
-	':full_name'=>$_POST[':full_name'],
-	':zip'=>intval($_POST[':zip']));
-	try {
+    
+    $array_ids = array(
+        ':street' => $_POST[':street'],
+        ':city' => $city,
+        ':full_name' => $full_name,
+        ':zip' => $zip
+    );
+    try {
         // These two statements run the query against your database table.
         $stmt   = $db->prepare($query);
         $result = $stmt->execute($array_ids);
     }
     catch (PDOException $ex) {
-		echo '{"FLAG":false,"ERR":3}';# Database error
+        echo '{"FLAG":false,"ERR":3}'; # Database error
         goto ERR;
     }
-    $row  = $stmt->fetch();
-	if($row){
-		echo '{"FLAG":true,"id":'.$row['driver_id'].'}';
-	}else{
-		$query = "
+    $row = $stmt->fetch();
+    if ($row) {
+		$return_id = $row['driver_id'];
+    } else {
+        $query     = "
             INSERT INTO Driver
             (street,city,full_name,zip)
             VALUES
 			(:street,:city,:full_name,:zip)";
-		$array_ids = array(':street'=>$_POST[':street'],
-	':city'=>$_POST[':city'],
-	':full_name'=>$_POST[':full_name'],
-	':zip'=>intval($_POST[':zip']));
-		try {
-			// These two statements run the query against your database table.
-			$stmt   = $db->prepare($query);
-			$result = $stmt->execute($array_ids);
-		}
-		catch (PDOException $ex) {
-			echo '{"FLAG":false,"ERR":3}';# Database error
-			goto ERR;
-		}
-		$temp = $stmt->fetch(PDO::FETCH_ASSOC);
-		echo '{"FLAG":true,"id":'.json_encode($temp).'}';
-		// insert grab key and send it back
-	}
-}
-else{
-	$query = "
+        $array_ids = array(
+            ':street' => $street,
+            ':city' => $city,
+            ':full_name' => $full_name,
+            ':zip' => $zip
+        );
+        try {
+            // These two statements run the query against your database table.
+            $stmt   = $db->prepare($query);
+            $result = $stmt->execute($array_ids);
+            $return_id   = $db->lastInsertId();
+        }
+        catch (PDOException $ex) {
+            echo '{"FLAG":false,"ERR":3}'; # Database error
+            goto ERR;
+        }
+        echo '{"FLAG":true,"id":' . json_encode($temp) . '}';
+        // insert grab key and send it back
+    }
+    // Updates junction table
+    $query     = "
+        INSERT INTO UserDrivers
+        (driver_id,user_id)
+        VALUES
+		(:driver_id,:user_id)";
+    $array_ids = array(
+        ':driver_id' => $return_id,
+        ':user_id' => $_SESSION['user']['user_id']
+    );
+    try {
+        // These two statements run the query against your database table.
+        $stmt   = $db->prepare($query);
+        $result = $stmt->execute($array_ids);
+    }
+    catch (PDOException $ex) {
+        if ($ex->getCode() != 23000) {
+            die('{"FLAG":false,"ERR":3,\"pdocode\":' . $ex->getCode() . '}'); # Database error
+        }
+    }
+    echo '{"FLAG":true,"id":' . strval($return_id) . '}';
+} else {
+    $query     = "
             SELECT
             *
             FROM Driver
 			WHERE
             driver_id=:driver_id";
-    $array_ids = array(':driver_id'=>$_POST[':driver_id']);
-	try {
+    $array_ids = array(
+        ':driver_id' => $d_id
+    );
+    try {
         // These two statements run the query against your database table.
         $stmt   = $db->prepare($query);
         $result = $stmt->execute($array_ids);
     }
     catch (PDOException $ex) {
-		echo '{"FLAG":false,"ERR":3}';# Database error
+        echo '{"FLAG":false,"ERR":3}'; # Database error
         goto ERR;
     }
-    $row  = $stmt->fetch();
-	if($row){
-		$query = "
+    
+    $row = $stmt->fetch();
+    if ($row) {
+        $query     = "
             UPDATE Driver
             SET street=:street, city=:city, full_name=:full_name, zip=:zip
             WHERE driver_id=:driver_id";
-    $array_ids = array(':street'=>$_POST[':street'],
-	':city'=>$_POST[':city'],
-	':full_name'=>$_POST[':full_name'],
-	':zip'=>intval($_POST[':zip']),
-	':driver_id'=>$_POST[':driver_id']);
-	try {
-        // These two statements run the query against your database table.
-        $stmt   = $db->prepare($query);
-        $result = $stmt->execute($array_ids);
-    }
-    catch (PDOException $ex) {
-		echo '{"FLAG":false,"ERR":3}';# Database error
+        $array_ids = array(
+            ':street' => $street,
+            ':city' => $city,
+            ':full_name' => $full_name,
+            ':zip' => intval($zip),
+            ':driver_id' => $d_id
+        );
+        try {
+            // These two statements run the query against your database table.
+            $stmt   = $db->prepare($query);
+            $result = $stmt->execute($array_ids);
+        }
+        catch (PDOException $ex) {
+            echo '{"FLAG":false,"ERR":3}'; # Database error
+            goto ERR;
+        }
+        echo '{"FLAG":true}';
+    } else {
+        echo '{"FLAG":false,"ERR":8}'; # Database error
         goto ERR;
     }
-	echo '{"FLAG":true}';
-	}else{
-		echo '{"FLAG":false,"ERR":8}';# Database error
-		goto ERR;
-	}
-}//driver id
-}else{ //main
-	echo '{"FLAG":false,"ERR":2}';# param error
-	goto ERR;
-}
+} //driver id
 die();
 ERR:
 header("HTTP/1.1 500 Internal Server Error");
