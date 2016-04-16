9b613a3510cc90ee3f43964243f4dd617f285464
bob135 <carter89@students.rowan.edu>
2016-04-16 15:49:53 -0400

    Displays more information in neater fashion

--- www/TableDisplay.php
+++ www/TableDisplay.php
@@ -1,21 +1,23 @@
-﻿<html>
+<html>
    
    <head>
       <title>Rowan Parking</title>
-      
+   </head>
+   <body>
       <style type = "text/css">
          body {
             font-family:Arial, Helvetica, sans-serif;
             font-size:14px;
          }
- 
+		 th,td{
+			 border-width:0px 1px 1px 0px;
+		 }
       </style>
       
-   </head>
-<a href="logout.php">Logout</a>
-</html>
+ <a href="logout.php">Logout</a>
 
-<?php
+
+ <?php
 
 require_once("common.php");
 
@@ -26,8 +28,8 @@ if (isset($_SESSION['username'])) {
     die("Redirecting..");
 }
 
-$query = "SELECT * FROM Requests Where status = 0 ORDER BY `time_stamp` DESC";
-
+//$query = "SELECT * FROM Requests Where status = 0 ORDER BY `start_date` ASC";
+$query = "SELECT Requests.*, Driver.full_name FROM Requests,Driver Where status = 0 AND Requests.driver_id = Driver.driver_id ORDER BY `start_date` ASC";
 try {
             // These two statements run the query against your database table.
             $stmt   = $db->prepare($query);
@@ -44,7 +46,7 @@ try {
 
 
      $row = $stmt->fetchAll();
-     echo json_encode($row[0]);
+     //echo json_encode($row[0]);
 
      echo nl2br("\n\n\n");
 
@@ -62,25 +64,34 @@ try {
 
 
 
-     echo "<tr>";
+     echo'<table border="1" ><th>Driver Name</th><th>Driver ID</th><th>Start Date</th><th>End Date</th><th>Request ID</th><th>View Job</th>';
      for($i = 0; $i < count($row); $i++) 
      {
-	$temp = $row[$i];
-	echo "test";
-
-
-
-        echo "<td>" . $temp['driver_id'] . "</td>\t";
-	echo "<td>" . $temp['start_date'] . "</td>\t";
-	echo "<td>" . $temp['end_date'] . "</td>\t";
-	echo "<td><a href='PassDetails.php?id=".$row['pass_id']."'>View Job</td></a>";
-	echo nl2br("\n");
+	$res = $row[$i];
+	echo'<tr><td>'.$res['full_name'].
+		'</td><td>'.$res['driver_id'].
+		'</td><td>'.$res['start_date'].
+		'</td><td>'.$res['end_date'].
+		'</td><td>'.$res['request_id'].
+		"</td><td><a href='PassDetails.php?id=".$res['request_id']."'>View Job</td></a></tr>";
+		//'</td><td><a href=PassDetails.php>View Job</a></td></tr>';
+		//'</td><td><a href=PassDetails.php?id= '.$res['request_id']. '>View Job</td></a></tr>';
+		
+	//echo "<tr><td>" . $temp['full_name'] . "</td>";
+    //echo "<td>" . $temp['driver_id'] . "</td>";
+	//echo "<td>" . $temp['start_date'] . "</td>";
+	//echo "<td>" . $temp['end_date'] . "</td>";
+	//echo "<td>" . $temp['request_id'] . "</td>";
+	//echo "<td><a href='PassDetails.php?id=".$temp['request_id']."'>View Job</td></a><\tr>";
+	//echo nl2br("\n");
      }
 
-        echo "</tr>";
+        echo '</table>';
 
 
 
 
-?>
+ ?>
+ </body>
+</html>
 
