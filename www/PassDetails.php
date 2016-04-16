64d81ce0069942aa397b57fe41a7707f6103e3b4
bob135 <carter89@students.rowan.edu>
2016-04-16 17:29:40 -0400

    Displays more viable information neater

--- www/PassDetails.php
+++ www/PassDetails.php
@@ -1,23 +1,167 @@
-PassDetails.php
+<html>
+   
+   <head>
+      <title>Rowan Parking</title>
+   </head>
+   <body>
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+		 th,td{
+			 border-width:0px 1px 1px 0px;
+		 }
+      </style>
+      
+ <a href="logout.php">Logout</a>
 
 <?php
-   include (‘Config.php’);
+   //PassDetails.php
+   require_once("common.php");
+   //include (‘Config.php’);
+
+if (isset($_SESSION['username'])) {
+    echo nl2br("\nWelcome to the member's area, " . $_SESSION['username'] . "!\n ");
+} else {
+    header("Location: login.php");
+    die("Redirecting..");
+}
+
+
+   $id = $_GET['id'];
+
+
+   $query = "SELECT Requests.*, Driver.*, Vehicles.*, User.* FROM Requests, Driver, Vehicles, User WHERE request_id = :id And 
+				Requests.driver_id = Driver.driver_id And Requests.vehicle_id = Vehicles.vehicle_id And Requests.user_id = User.user_id";
+   $query_params = array(':id' => $_GET['id']);
+
+   try {
+            // These two statements run the query against your database table.
+            $stmt   = $db->prepare($query);
+            $result = $stmt->execute($query_params);
+	    echo "Query Success";
+        }
+        catch (PDOException $ex) {
+            // Note: On a production website, you should not output $ex->getMessage().
+            // It may provide an attacker with helpful information about your code. 
+            die("Failed to run query: " . $ex->getMessage());
+        }
+   $row = $stmt->fetchAll();
+   //echo json_encode($row[0]);
+   //$result = mysqli_query($conn,"SELECT * FROM passTable WHERE pass_id = '".$_GET['id']."' ORDER BY `CreatedTime` DESC");
+
+   $passdetails = $row[0];
+
+   if($passdetails['status'] != 0 || $passdetails == null)
+   {
+       header("Location: TableDisplay.php");
+       die("Redirecting..");
+   }
+
+   echo '<br>';
    
-   $result = mysqli_query($conn,"SELECT * FROM passTable WHERE pass_id = '".$_GET['id']."' ORDER BY `CreatedTime` DESC");
+		
+	echo'<table border="1" >
+	<tr><td><th>Request ID</th><td>'.$passdetails['request_id'] . ' </td></tr>
+	<tr><td><th>User Account</th><td>'.$passdetails['user_name'] . ' </td></tr>
+	<tr><td><th>Pass Start Date</th><td>'.$passdetails['start_date'] . ' </td></tr>
+	<tr><td><th>Pass End Date</th><td>'.$passdetails['end_date'] . ' </td></tr>
+	<tr><td><th>Driver Name</th><td>'.$passdetails['full_name'] . ' </td></tr>
+	<tr><td><th>Driver Street</th><td>'.$passdetails['street'] . ' </td></tr>
+	<tr><td><th>Driver City</th><td>'.$passdetails['city'] . ' </td></tr>
+	<tr><td><th>Driver State</th><td>'.$passdetails['Driver.state'] . ' </td></tr>
+	<tr><td><th>Driver Zip</th><td>'.$passdetails['zip'] . ' </td></tr>
+	<tr><td><th>Vehicle Color</th><td>'.$passdetails['color'] . ' </td></tr>
+	<tr><td><th>Vehicle Year</th><td>'.$passdetails['year'] . ' </td></tr>
+	<tr><td><th>Vehicle Make</th><td>'.$passdetails['make'] . ' </td></tr>
+	<tr><td><th>Vehicle Model</th><td>'.$passdetails['model'] . ' </td></tr>
+	<tr><td><th>Vehicle State</th><td>'.$passdetails['Vehicles.state'] . ' </td></tr>
+	<tr><td><th>Vehicle License</th><td>'.$passdetails['license'] . ' </td></tr>';
 
-   $passdetails = mysqli_fetch_assoc($result);
+        echo '</table>';
+$full_email = $passdetails['user_name'] . "@students.rowan.edu";
+if(isset($_POST['accept_button_pressed']))
+{
+    echo "Approve";
+    $status = 1;
+    $query = "UPDATE Requests ". "SET status=$status "."WHERE request_id = :id";
+    $query_params = array(':id' => $_GET['id']);
 
-   echo 'Pass ID : '.$passdetails['pass_id'].'<br>';
-   echo 'Driver Name: '.$passdetails['driver_name'].'<br>';
-   echo 'Location: '.$passdetails['location'].'<br>';
-   echo Vehicle Year: '.$passdetails['year'].'<br>';
-   echo Vehicle Make: '.$passdetails['make'].'<br>';
-   echo Vehicle Model: '.$passdetails['model'].'<br>';
-   echo License Plate: '.$passdetails['license'].'<br>';
-   echo Start Date: '.$passdetails['start_date'].'<br>';
-   echo End Date: '.$passdetails['end_date'].'<br>';
+    try {
+            // These two statements run the query against your database table.
+            $stmt   = $db->prepare($query);
+            $result = $stmt->execute($query_params);
+			//echo "Query Success";
+			echo nl2br("\n");
+			/*$retval = mysql_query($sql, $db);
+			$to      = $full_email;
+			$subject = 'Rowan Parking Pass';
+			$message = 'hello here’s your nice new shiny pass.';
+			$headers = 'From: webmaster@example.com' . "\r\n" .
+			'Reply-To: webmaster@example.com' . "\r\n" .
+			'X-Mailer: PHP/' . phpversion();
+			mail($to, $subject, $message, $headers);
+			echo 'Pass Accepted. Email Sent.';
+			header(‘refresh:5; Location: TableDisplay’);
+			exit; */
+			
+			//echo 'Pass Accepted. Email Sent.';
+			//echo "<script>setTimeout(\"location.href = 'TableDisplay.php';\",3000);</script>";
+			//sleep(3);
+			//header("Location: TableDisplay.php");
+			//exit;
+        }
+        catch (PDOException $ex) {
+            // Note: On a production website, you should not output $ex->getMessage().
+            // It may provide an attacker with helpful information about your code. 
+            die("Failed to run query: " . $ex->getMessage());
+        }
 
 
+    header("Location: TableDisplay.php");
+    die("Redirecting..");
+}
+
+if(isset($_POST['deny_button_pressed']))
+{
+    echo "DENY";
+    $status = 2;
+    $query = "UPDATE Requests ". "SET status=$status "."WHERE request_id = :id";
+    $query_params = array(':id' => $_GET['id']);
+
+    try {
+            // These two statements run the query against your database table.
+            $stmt   = $db->prepare($query);
+            $result = $stmt->execute($query_params);
+			echo "Query Success";
+			
+			/*$retval = mysql_query($sql, $db);
+			$to      = $full_email;
+			$subject = 'Rowan Parking Pass';
+			$message = 'Your pass has been denied';
+			$headers = 'From: webmaster@example.com' . "\r\n" .
+			'Reply-To: webmaster@example.com' . "\r\n" .
+			'X-Mailer: PHP/' . phpversion();
+			mail($to, $subject, $message, $headers);
+			echo 'Pass Accepted. Email Sent.';
+			header(‘refresh:5; Location: TableDisplay’);
+			exit; */
+        }
+        catch (PDOException $ex) {
+            // Note: On a production website, you should not output $ex->getMessage().
+            // It may provide an attacker with helpful information about your code. 
+            die("Failed to run query: " . $ex->getMessage());
+        }
+
+     header("Location: TableDisplay.php");
+    die("Redirecting..");
+
+}
+?>
+</body>
+</html>
+<html>
 <form action="" method="post">
     <input type="submit" value="Accept" />
     <input type="hidden" name="accept_button_pressed" value="1" />
@@ -27,56 +171,4 @@ PassDetails.php
     <input type="submit" value="Deny" />
     <input type="hidden" name="deny_button_pressed" value="1" />
 </form>
-
-if(isset($_POST['accept_button_pressed']))
-{
-    $sql = “UPDATE passTable “. “SET pass_type=accepted”.“WHERE pass_id = $passdetails[‘id’]”;
-    $retval = mysql_query($sql, $db);
-    $to      = 'nobody@example.com';
-    $subject = 'the subject';
-    $message = 'hello here’s your nice new shiny pass.';
-    $headers = 'From: webmaster@example.com' . "\r\n" .
-        'Reply-To: webmaster@example.com' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $message, $headers);
-
-    echo 'Pass Accepted. Email Sent.';
-    header(‘refresh:5; Location: TableDisplay’);
-    exit; 
-}
-
-if(isset($_POST['deny_button_pressed']))
-{
-    $sql = “UPDATE passTable “. “SET pass_type=denied”.“WHERE pass_id = $passdetails[‘id’]”;
-    $retval = mysql_query($sql, $db);
-
-    $to      = 'nobody@example.com';
-    $subject = 'the subject';
-    $message = 'Your parking pass for bla bla date has been denied.';
-    $headers = 'From: webmaster@example.com' . "\r\n" .
-        'Reply-To: webmaster@example.com' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $message, $headers);
-    echo 'Pass Denied. Email Sent.';
-    header(‘refresh:5; Location: TableDisplay’);
-    exit;
-}
-?>
-<html>
-   
-   <head>
-      <title>Rowan Parking</title>
-      
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
- 
-      </style>
-      
-   </head>
-<a href="logout.php">Logout</a>
 </html>
