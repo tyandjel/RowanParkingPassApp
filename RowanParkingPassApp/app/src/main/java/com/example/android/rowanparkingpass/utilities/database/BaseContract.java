9c2501d5c3317b0f5313a8ea6de0df3b41d2d3df
tyandjel94 <tyandjel94@gmail.com>
2016-04-30 18:43:27 -0400

    Blop to Blob

--- RowanParkingPassApp/app/src/main/java/com/example/android/rowanparkingpass/utilities/database/BaseContract.java
+++ RowanParkingPassApp/app/src/main/java/com/example/android/rowanparkingpass/utilities/database/BaseContract.java
@@ -5,7 +5,7 @@ public abstract class BaseContract {
     public static final String TEXT_TYPE = " TEXT";
     public static final String INTEGER_TYPE = " INTEGER";
     public static final String DATE_TYPE = " DATE";
-    public static final String BLOB = " BLOP";
+    public static final String BLOB = " BLOB";
     public static final String UNIQUE_TYPE = " UNIQUE";
     public static final String COMMA_SEP = ", ";
     public static final String WHERE = " WHERE";
